d840521256dd2ad996d7f4a62508d6fc66689102
goodsmell <87801306+goodsmell@users.noreply.github.com>
2023-06-18 03:20:46 +0900

    물체 잡으면 돈 올라감 ( 물고기만 되게 수정해야함)

---
+++ Assets/Scenes/eunhyang/money.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class money : MonoBehaviour
+
+{
+
+    TMP_Text myMoney;
+    public int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hell");
+        myMoney = GameObject.Find("Money").GetComponent<TextMeshProUGUI>();
+        myMoney.text = "$ " + count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMoney.text = "$ " + count;
+    }
+
+    /*void plusMoney()
+    {
+        count += 100;
+        //사운드 추가
+    }*/
+    
+    public void select() // select 됐을 떄
+    {
+        
+        count += 100;
+        Debug.Log(count);
+        //other.gameObject.SetActive(false); //아이템 비활성화 
+        /*if(other.tag == "fish"){
+
+                    count += 100;
+                    other.gameObject.SetActive(false); //아이템 비활성화 
+
+                }*/
+    }
+
+   /* void OnTriggerEnter(Collider other)
+    { // select 됐을 때
+    Debug.Log("trigger");
+    if (other.tag == "fish"){
+
+                count += 100;
+        Debug.Log(count);
+        other.gameObject.SetActive(false); //아이템 비활성화 
+
+            }
+
+}*/
+
+
+
+}
